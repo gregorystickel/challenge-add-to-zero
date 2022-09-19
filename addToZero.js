b3c6640b1530c3b4fba68dd902467185a533ba41
Greg Stickel <gregory.stickel@live.com>
2022-09-19 09:47:12 -0400

    Completed

--- addToZero.js
+++ addToZero.js
@@ -4,18 +4,16 @@ let array = [28, 43, -12, 30, 4, 0, 12];
 // Write your solution below:
 
 let arrNum = [1, 4, 11, 2, 37, -4];
+let result = false;
 
 for (let i = 0; i < arrNum.length; i++) {
   for (let j = 0; j < arrNum.length; j++) {
-    if ( i != j) {
-    let diff = arrNum[i] + arrNum[j];
-    if (diff === 0) {
-      //console.log(`${arrNum[i]} - ${arrNum[j]} = ${diff} true`);
-      console.log(true);
-    } else {
-      //console.log(`${arrNum[i]} - ${arrNum[j]} = ${diff} false`);
-      console.log(false);
+    if (i != j) {
+      let diff = arrNum[i] + arrNum[j];
+      if (diff === 0) {
+        result = true;
+      }
     }
-}
   }
 }
+console.log(result);
