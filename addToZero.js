b80074ae0cd9642b6406438a39dfb11879a11572
Greg Stickel <gregory.stickel@live.com>
2022-09-19 10:00:41 -0400

    Updated

--- addToZero.js
+++ addToZero.js
@@ -1,16 +1,15 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, 12];
+let array = [0, 1, 2, 3, 4, 5];
 
 // Write your solution below:
 // Find if any two numbers add to 0
 
-let arrNum = [1, 4, 11, 2, 37, -4];
 let result = false;
 
-for (let i = 0; i < arrNum.length; i++) {
-  for (let j = 0; j < arrNum.length; j++) {
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
     if (i != j) {
-      let diff = arrNum[i] + arrNum[j];
+      let diff = array[i] + array[j];
       if (diff === 0) {
         result = true;
       }
